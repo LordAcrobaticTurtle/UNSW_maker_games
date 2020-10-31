e2b92e0893dba0eb99d8831ada6418c9637d3e0e
TL-001 <47770984+TL-001@users.noreply.github.com>
2020-11-01 08:56:28 +1100

    small change to gps init step & print lines

--- UNSW_maker_games.ino
+++ UNSW_maker_games.ino
@@ -1,3 +1,5 @@
+// UNSW Maker Games 2020 code
+
 #include "Wire.h"
 #include <MPU6050_light.h>
 #include <TinyGPS++.h>
@@ -6,16 +8,6 @@
 #include "serial_input_control.h"
 #include "DeadReckoner_IMU.h"
 #include "GPS2Local.h"
-
-
-// UNSW Maker Games code
-
-unsigned long prevPositionComputeTime = 0;
-#define POSITION_COMPUTE_INTERVAL 50 // milliseconds
-#define SEND_INTERVAL 100 // milliseconds
-
-//static const int RXPin = 50, TXPin = 51;
-static const uint32_t GPSBaud = 9600;
 
 // IMU object
 MPU6050 mpu(Wire);
@@ -29,8 +21,8 @@ TinyGPSPlus gps;
 // GPS to local object
 GPS2Local GPS2Local;
 
-// The software serial connection to the GPS device
-//SoftwareSerial ss(RXPin, TXPin);
+// Variables
+unsigned long prevPositionComputeTime = 0;
 
 void setup() {
 	// put your setup code here, to run once:
@@ -38,21 +30,17 @@ void setup() {
 	for (int i = 2; i <= 13; i++) {
 		pinMode(i, OUTPUT); 
 	}
-	// This configuration makes right turns only 
-
-
-	Serial.println("WAITING ON INPUT");
 
 	// IMU setup
 	Wire.begin();
 	mpu.begin();
-	Serial.println(F("Calculating gyro offset, do not move MPU6050"));
+	Serial.print(F("Calculating gyro offset, do not move MPU6050"));
 	delay(1000);
 	mpu.calcGyroOffsets();
-	Serial.println("Done!\n");
+	Serial.println("Done!");
 
 	// Calculating IMU bias
-	Serial.println(F("Calculating bias, do not move MPU6050"));
+	Serial.print(F("Calculating bias, do not move MPU6050"));
 	for (int i = 0; i<200; i++) {
 		mpu.update();
 		//DeadReckoner.update(mpu);
@@ -60,20 +48,23 @@ void setup() {
   
 		delay(20);
 	}
-	Serial.println("Done!\n");
+	Serial.println("Done!");
 	
 	// GPS setup
-	Serial5.begin(GPSBaud);
-	Serial.println(F("Calculating GPS position, do not move"));
-	unsigned long start  = millis();
-	while (millis() - start < 5000)
+	Serial5.begin(9600);
+	Serial.print(F("Calculating GPS position, do not move"));
+	//unsigned long start  = millis();
+	//while (millis() - start < 5000)
+	do {
 		while (Serial5.available() > 0)
 			if (gps.encode(Serial5.read()))
 				GPS2Local.GPSdisplayRawInfo(gps);
+	}
+	while(~gps.location.isUpdated());
 	
 	GPS2Local.init(gps);
 	
-	Serial.println("Done!\n");
+	Serial.println("Done!");
   
 	// Timers init
 	DeadReckoner.init_timers();
@@ -81,18 +72,13 @@ void setup() {
 
 void loop() {
 	// put your main code here, to run repeatedly:
-	serial_input_control(Serial);
   
 	// IMU Dead Reckoning
-	if (millis() - prevPositionComputeTime > POSITION_COMPUTE_INTERVAL) {
+	if (millis() - prevPositionComputeTime > 50) {	// milliseconds
 		mpu.update();
 		
-		//DeadReckoner.update(mpu);
 		DeadReckoner.compute_velocity(mpu);
 		DeadReckoner.compute_position(mpu);
-		
-		Serial.print("IMU X: ");
-		Serial.print(String(DeadReckoner.getX()) + " ");
 		
 		prevPositionComputeTime = millis();
 	}
@@ -100,12 +86,18 @@ void loop() {
 	// GPS local positioning
 	while (Serial5.available() > 0)
 		if (gps.encode(Serial5.read()))
-			GPS2Local.GPSdisplayRawInfo(gps);
+			//GPS2Local.GPSdisplayRawInfo(gps);
 	
-	GPS2Local.computeLocal(gps);
+	if (gps.location.isUpdated())
+		GPS2Local.computeLocal(gps);
 
-	Serial.print("GPS X: ");
-	Serial.println(GPS2Local.GetLocalX());
+	Serial.print("IMU X: "); Serial.print(DeadReckoner.getX());
+	Serial.print("IMU Y: "); Serial.print(DeadReckoner.getY()); 
+	Serial.print("IMU Heading: "); Serial.print(DeadReckoner.getHeading()); Serial.print("\t");
+	Serial.print("GPS X: "); Serial.print(GPS2Local.GetLocalX());
+	Serial.print("GPS Y: "); Serial.print(GPS2Local.GetLocalY());
+	
+
 
 	delay(20);
 }
